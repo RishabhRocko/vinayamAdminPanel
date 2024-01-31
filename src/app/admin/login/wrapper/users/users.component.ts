58fb5ec4cb6b0f53ff1505c9cb1ffa5b80b07cf9
Rishabh Mainwal <mainwalrishabh@gmail.com>
2024-01-31 10:06:51 +0530

    student get and delete api implemented

--- src/app/admin/login/wrapper/users/users.component.ts
+++ src/app/admin/login/wrapper/users/users.component.ts
@@ -1,8 +1,65 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { UsersService } from './users.service';
+import { ToastrService } from 'ngx-toastr';
+import { decryptData } from 'src/app/helper/cryptoEncryption';
 
 @Component({
   selector: 'user-wrapper',
   templateUrl: './users.component.html',
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  landData:any;
+  sendData:any;
+  token:any;
+  deleteId:any;
+  deleteEncString:any;
+
+  constructor(private UsersService:UsersService,private toastr: ToastrService){}
+  ngOnInit(): void {
+    this.token = localStorage.getItem('token');
+    this.sendData = {"token":this.token};
+    this.UsersService.allAdminStudents(this.sendData).subscribe((res: any) => {
+      let response = decryptData(res);
+      if(response.status == true)
+      {
+        this.landData = response.data;
+      }else{
+        this.toastr.error(response.message ? response.message : 'Error', 'Error', {
+          positionClass: 'errorMessageClass'
+       });
+      }
+    });
+  }
+
+  getDeleteData(id: any,encString: any)
+  {
+    this.deleteId = id;
+    this.deleteEncString = encString;
+  }
+
+  deleteStudent(isValid:any)
+  {
+    if(isValid && this.deleteEncString != localStorage.getItem('token'))
+    {
+      this.sendData = {"token":this.token,"deleteId":this.deleteId,"deleteEncString":this.deleteEncString};
+      this.UsersService.deleteStudentData(this.sendData).subscribe((res: any) => {
+        let response = decryptData(res);
+        if(response.status == true)
+        {
+          this.toastr.success(response.message ? response.message : 'Success', 'Success', {
+            positionClass: 'successMessageClass'
+         });
+         this.ngOnInit();
+        }else{
+          this.toastr.error(response.message ? response.message : 'Error', 'Error', {
+            positionClass: 'errorMessageClass'
+         });
+        }
+      });
+    }else{
+      this.toastr.error('Can`t Delete Student!', 'Error', {
+        positionClass: 'errorMessageClass'
+     });
+    }
+  }
 }
---
+++ src/app/admin/login/wrapper/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { encryptData } from 'src/app/helper/cryptoEncryption';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http:HttpClient) { }
+
+  allAdminStudents(sendData:any)
+  {
+    let url = environment.url + 'adminStudents';
+    let encData:string = encryptData(sendData);
+    return this.http.post<any>(url,encData);
+  }
+
+  deleteStudentData(sendData:any)
+  {
+    let url = environment.url + 'adminStudentDelete';
+    let encData:string = encryptData(sendData);
+    return this.http.post<any>(url,encData);
+  }
+}
