1d3cf0bc9fda9eaa686e2b4213b44580e54ab3e4
Rishabh Mainwal <mainwalrishabh@gmail.com>
2024-03-21 00:05:48 +0530

    weeb template added

--- src/app/web/web.component.ts
+++ src/app/web/web.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { WebService } from './web.service';
+import { ToastrService } from 'ngx-toastr';
+import { decryptData } from 'src/app/helper/cryptoEncryption';
 @Component({
   selector: 'app-web',
   templateUrl: './web.component.html',
 })
-export class WebComponent {
-
+export class WebComponent implements OnInit{
+  user: any;
+  constructor(private WebService:WebService,private router: Router,private toastr: ToastrService){}
+  ngOnInit(): void {
+    localStorage.removeItem("token");
+  }
+  onAdmin()
+  {
+    this.router.navigate(['/admin']);
+  }
 }
